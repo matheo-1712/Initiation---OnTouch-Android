b7dab966f872ee90951124439dc6f76736c32123
matheo-1712 <perod.matheo@gmail.com>
2026-01-19 16:30:18 +0100

    Implement box movement on touch move event & touch item

--- app/src/main/java/com/example/tp2ex1/MainActivity.java
+++ app/src/main/java/com/example/tp2ex1/MainActivity.java
@@ -27,11 +27,29 @@ public class MainActivity extends AppCompatActivity {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         Log.d(DEBUG_TAG, "Action was DOWN");
-                        // On déplace le carré à la position du doigt
-                        box.setX(event.getX() - (float) box.getWidth() / 2);
-                        box.setY(event.getY() - (float) box.getHeight() / 2);
 
-                        return true; // Important pour continuer à recevoir les autres événements
+                        float touchX = event.getX();
+                        float touchY = event.getY();
+
+                        // Vérifie si le toucher est à l'intérieur du carré
+                        if (touchX >= box.getX() && touchX <= box.getX() + box.getWidth()
+                                && touchY >= box.getY() && touchY <= box.getY() + box.getHeight()) {
+
+                            Log.d(DEBUG_TAG, "Touch was inside the box");
+
+                            // On déplace le carré à la position du doigt, centré
+                            box.setX(touchX - box.getWidth() / 2f);
+                            box.setY(touchY - box.getHeight() / 2f);
+
+                            // On autorise le mouvement
+                            return true;
+
+                        } else {
+                            Log.d(DEBUG_TAG, "Touch was outside the box");
+                        }
+
+                        return false; // on ne consomme pas l'événement si on n'a pas touché le carré
+
                     case MotionEvent.ACTION_MOVE:
                         Log.d(DEBUG_TAG, "Action was MOVE");
                         // On déplace le carré à la position du doigt
